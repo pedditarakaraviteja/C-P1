9ddd2c23dbddee88cd51973ac863b4c427256821
pedditarakaraviteja <pedditarakaraviteja@gmail.com>
2019-01-24 18:54:16 -0600

    3rd commit

--- p1/Program.cs
+++ p1/Program.cs
@@ -11,6 +11,7 @@ namespace p1
             var c=true;
             String brand;
             decimal cost;
+            int quantity;
 
             Console.WriteLine("Enter the name");
             var f= Console.ReadLine();
@@ -22,19 +23,27 @@ namespace p1
             var e=Console.ReadLine();
             Console.WriteLine($"{e} Gallons");
             Console.WriteLine("Enter the brand label need to buy ");
-            brand =Console.ReadLine();
             
-
+            Console.WriteLine("Nestle - 1.5$/gallon");
+             Console.WriteLine(" Pure - 2.5$/gallon");
+             Console.WriteLine("Aqua - 3$/gallon");
+             Console.WriteLine("please select the brand");
+            brand =Console.ReadLine();
             switch(brand){
                 case "Nestle": Console.WriteLine($"you selected the Nestle {e} gallon");
                 break;
-                case "Pure": Console.WriteLine($"You selected the pure {e} gallon");
+                case "pure": Console.WriteLine($"You selected the pure {e} gallon");
                 break;
                 case "Aqua": Console.WriteLine($" you selected the Aqua {e} gallon");
                 break;
                 default: Console.WriteLine("Please select ");
                 break;
             }
+            Console.WriteLine($"How many {e} gallons you need");
+             quantity=Convert.ToInt32(Console.ReadLine());
+             
+
+
 
             
 
