10bbfde7892a247c8766a46db27a5bdceb3973a9
pedditarakaraviteja <pedditarakaraviteja@gmail.com>
2019-01-24 18:40:21 -0600

    SECOND

--- p1/Program.cs
+++ p1/Program.cs
@@ -9,18 +9,33 @@ namespace p1
             var a=0;
             var b=0.0;
             var c=true;
+            String brand;
+            decimal cost;
 
             Console.WriteLine("Enter the name");
             var f= Console.ReadLine();
+            while(f==""){
+                f= Console.ReadLine();
+            }
             Console.WriteLine($"Welcome {f}");
             Console.WriteLine("Enter the Bottle Size");
             var e=Console.ReadLine();
             Console.WriteLine($"{e} Gallons");
-//             if(e==5)
-//             {
-//  Console.WriteLine("the price for 5 gallons is 1.5 USD");
-//             }
-           
+            Console.WriteLine("Enter the brand label need to buy ");
+            brand =Console.ReadLine();
+            
+
+            switch(brand){
+                case "Nestle": Console.WriteLine($"you selected the Nestle {e} gallon");
+                break;
+                case "Pure": Console.WriteLine($"You selected the pure {e} gallon");
+                break;
+                case "Aqua": Console.WriteLine($" you selected the Aqua {e} gallon");
+                break;
+                default: Console.WriteLine("Please select ");
+                break;
+            }
+
             
 
             
