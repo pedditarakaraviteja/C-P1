fd039129c480c857d8c86e6a9adac898e86abb03
pedditarakaraviteja <pedditarakaraviteja@gmail.com>
2019-01-24 18:13:19 -0600

    FIRST

---
+++ p1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a=0;
+            var b=0.0;
+            var c=true;
+
+            Console.WriteLine("Enter the name");
+            var f= Console.ReadLine();
+            Console.WriteLine($"Welcome {f}");
+            Console.WriteLine("Enter the Bottle Size");
+            var e=Console.ReadLine();
+            Console.WriteLine($"{e} Gallons");
+//             if(e==5)
+//             {
+//  Console.WriteLine("the price for 5 gallons is 1.5 USD");
+//             }
+           
+            
+
+            
+        }
+    }
+}
